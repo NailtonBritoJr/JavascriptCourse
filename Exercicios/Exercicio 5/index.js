0127b3dc7ccad5bbd9a8dd9cc298968344fe41d4
Nailton Brito Junior <nailton.brito.2003@gmail.com>
2024-03-12 18:35:47 -0300

    Exercicio 5

---
+++ Exercicios/Exercicio 5/index.js
@@ -0,0 +1,11 @@
+const nome = prompt('Digite seu nome: ');
+
+document.body.innerHTML += `Seu nome completo é: "nome" <br />`
+document.body.innerHTML += `Seu nome tem: "quantia" letras. <br />`
+document.body.innerHTML += `A segunda letra do seu nome é: "Letra". <br />`
+document.body.innerHTML += `Qual o primeiro índice da letra a no seu nome? <br />`
+document.body.innerHTML += `Qual o último índice da letra a no seu nome? <br /> `
+document.body.innerHTML += `As últimas 3 letras do seu nome são: <br />`
+document.body.innerHTML += `As palavras do seu nome são: <br />`
+document.body.innerHTML += `Seu nome com letras maiúsculas: <br />`
+document.body.innerHTML += `Seu nome com letras minúsculas: <br />`
