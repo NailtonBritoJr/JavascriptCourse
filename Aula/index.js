bfd393491bc84bff6c79339feb0dfbfba18bb65b
Nailton Brito Junior <nailton.brito.2003@gmail.com>
2024-03-12 20:30:52 -0300

    Aula 25

--- Aula/index.js
+++ Aula/index.js
@@ -1,1 +1,12 @@
-console.log('Nailton Brito');
+let num1 = 15.059159289; //number
+let num2 = 2.5; //number
+
+//console.log(num1.toString() + num2);
+//console.log(typeof num1);
+//num1 = num1.toString();
+//console.log = (num1.toString(2));
+//console.log = (num1.toFixed(2));
+//console.log = (Number.isInteger(num1));
+let temp = num1 * '5';
+//console.log(Number.isNaN(temp));
+//console.log(temp);
