6b7e14bbb5f65be83491e88c596baca1966c8ee6
Nailton Brito Junior <nailton.brito.2003@gmail.com>
2024-03-19 18:31:13 -0300

    Metodo Math

---
+++ Aula/app.js
@@ -0,0 +1,7 @@
+
+/*let num2 = Math.floor(num1);*/ // Arrendondar para baixo
+/*let num2 = Math.ceil(num1);*/ //Arrendonda pra cima.
+//console.log(Math.min/max) pega o maior ou menor numero da sequencia.
+//console.log(math.random) Numero aleatorio.
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+//console.log(num1 ** (1/2)); Raiz Quadrada.
